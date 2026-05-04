2853a531aff62887bd2184e9ca45c1b660dab17a
Faddey Pozdeev <faddey2012@yandex.ru>
2026-05-04 17:10:56 +0300

    Initial commit

---
+++ src/main/java/ru/project/h2otesttask/H2oTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.project.h2otesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2oTestTaskApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(H2oTestTaskApplication.class, args);
+  }
+
+}
